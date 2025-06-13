5af98f5c7d026f13812b8d828a82b093b00bbb35
dariantonova <daricio1256@gmail.com>
2025-06-13 12:29:05 +0300

    Add comments wrap controller and methods to posts controller

---
+++ src/features/blogger-platform-wrap/comments/api/comments.controller.wrap.ts
@@ -0,0 +1,82 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { JwtAccessOptionalAuthGuardWrap } from '../../../user-accounts-wrap/guards/bearer/jwt-access-optional-auth.guard.wrap';
+import { JwtAccessAuthGuardWrap } from '../../../user-accounts-wrap/guards/bearer/jwt-access-auth.guard.wrap';
+import { ExtractUserIfExistsFromRequest } from '../../../user-accounts/guards/decorators/param/extract-user-if-exists-from-request';
+import { UserContextDto } from '../../../user-accounts/guards/dto/user-context.dto';
+import { CommentViewDto } from '../../../blogger-platform/comments/api/view-dto/comments.view-dto';
+import { ExtractUserFromRequest } from '../../../user-accounts/guards/decorators/param/extract-user-from-request';
+import { UpdateCommentInputDto } from '../../../blogger-platform/comments/api/input-dto/update-comment.input-dto';
+import { IntValidationPipe } from '../../../../core/pipes/int-validation-pipe';
+import { GetCommentByIdOrNotFoundFailQueryWrap } from '../application/queries/get-comment-by-id-or-not-found-fail.query.wrap';
+import { UpdateCommentCommandWrap } from '../application/usecases/update-comment.usecase.wrap';
+import { DeleteCommentCommandWrap } from '../application/usecases/delete-comment.usecase.wrap';
+
+@Controller('comments')
+export class CommentsControllerWrap {
+  constructor(
+    private queryBus: QueryBus,
+    private commandBus: CommandBus,
+  ) {}
+
+  @Get(':id')
+  @UseGuards(JwtAccessOptionalAuthGuardWrap)
+  async getComment(
+    @Param('id', IntValidationPipe) id: string,
+    @ExtractUserIfExistsFromRequest() user: UserContextDto | null,
+  ): Promise<CommentViewDto> {
+    return this.queryBus.execute(
+      new GetCommentByIdOrNotFoundFailQueryWrap(id, user?.id),
+    );
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAccessAuthGuardWrap)
+  async updateComment(
+    @ExtractUserFromRequest() user: UserContextDto,
+    @Param('id', IntValidationPipe) id: string,
+    @Body() body: UpdateCommentInputDto,
+  ): Promise<void> {
+    await this.commandBus.execute(
+      new UpdateCommentCommandWrap(id, body, user.id),
+    );
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @UseGuards(JwtAccessAuthGuardWrap)
+  async deleteComment(
+    @ExtractUserFromRequest() user: UserContextDto,
+    @Param('id', IntValidationPipe) id: string,
+  ): Promise<void> {
+    await this.commandBus.execute(new DeleteCommentCommandWrap(id, user.id));
+  }
+
+  // @Put(':commentId/like-status')
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  // @UseGuards(JwtAccessAuthGuard)
+  // async makeCommentLikeOperation(
+  //   @ExtractUserFromRequest() user: UserContextDto,
+  //   @Param('commentId', ObjectIdValidationPipe) commentId: string,
+  //   @Body() body: LikeInputDto,
+  // ): Promise<void> {
+  //   await this.commandBus.execute(
+  //     new MakeCommentLikeOperationCommand({
+  //       commentId,
+  //       userId: user.id,
+  //       likeStatus: body.likeStatus,
+  //     }),
+  //   );
+  // }
+}
--- src/features/blogger-platform-wrap/posts/api/posts.controller.wrap.ts
+++ src/features/blogger-platform-wrap/posts/api/posts.controller.wrap.ts
@@ -1,5 +1,13 @@
-import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { JwtAccessOptionalAuthGuardWrap } from '../../../user-accounts-wrap/guards/bearer/jwt-access-optional-auth.guard.wrap';
 import { GetPostsQueryParams } from '../../../blogger-platform/posts/api/input-dto/get-posts-query-params.input-dto';
 import { UserContextDto } from '../../../user-accounts/guards/dto/user-context.dto';
@@ -9,10 +17,21 @@ import { GetPostsQueryWrap } from '../application/queries/get-posts.query.wrap';
 import { ExtractUserIfExistsFromRequest } from '../../../user-accounts/guards/decorators/param/extract-user-if-exists-from-request';
 import { IntValidationPipe } from '../../../../core/pipes/int-validation-pipe';
 import { GetPostByIdOrNotFoundFailQueryWrap } from '../application/queries/get-post-by-id-or-not-found-fail.query.wrap';
+import { JwtAccessAuthGuardWrap } from '../../../user-accounts-wrap/guards/bearer/jwt-access-auth.guard.wrap';
+import { ExtractUserFromRequest } from '../../../user-accounts/guards/decorators/param/extract-user-from-request';
+import { CreatePostCommentInputDto } from '../../../blogger-platform/posts/api/input-dto/create-post-comment.input-dto';
+import { CommentViewDto } from '../../../blogger-platform/comments/api/view-dto/comments.view-dto';
+import { GetCommentsQueryParams } from '../../../blogger-platform/comments/api/input-dto/get-comments-query-params.input-dto';
+import { GetPostCommentsQueryWrap } from '../../comments/application/queries/get-post-comments.query.wrap';
+import { CreateCommentCommandWrap } from '../../comments/application/usecases/create-comment.usecase.wrap';
+import { GetCommentByIdOrInternalFailQueryWrap } from '../../comments/application/queries/get-comment-by-id-or-internal-fail.query.wrap';
 
 @Controller('posts')
 export class PostsControllerWrap {
-  constructor(private queryBus: QueryBus) {}
+  constructor(
+    private queryBus: QueryBus,
+    private commandBus: CommandBus,
+  ) {}
 
   @Get()
   @UseGuards(JwtAccessOptionalAuthGuardWrap)
@@ -35,49 +54,43 @@ export class PostsControllerWrap {
     );
   }
 
-  // @Get(':postId/comments')
-  // @UseGuards(JwtAccessOptionalAuthGuardSql)
-  // async getPostComments(
-  //   @Param(
-  //     'postId',
-  //     new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_FOUND }),
-  //   )
-  //     postId: number,
-  //   @Query() query: GetCommentsQueryParams,
-  //   @ExtractUserIfExistsFromRequestSql() user: UserContextDtoSql | null,
-  // ): Promise<PaginatedViewDto<CommentViewDto[]>> {
-  //   return this.queryBus.execute(
-  //     new GetPostCommentsQuerySql(postId, query, user?.id),
-  //   );
-  // }
-  //
-  // @Post(':postId/comments')
-  // @UseGuards(JwtAccessAuthGuardSql)
-  // async createPostComment(
-  //   @ExtractUserFromRequestSql() user: UserContextDtoSql,
-  //   @Param(
-  //     'postId',
-  //     new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_FOUND }),
-  //   )
-  //     postId: number,
-  //   @Body() body: CreatePostCommentInputDto,
-  // ): Promise<CommentViewDto> {
-  //   const createdCommentId = await this.commandBus.execute<
-  //     CreateCommentCommandSql,
-  //     number
-  //   >(
-  //     new CreateCommentCommandSql({
-  //       content: body.content,
-  //       postId,
-  //       userId: user.id,
-  //     }),
-  //   );
-  //
-  //   return this.queryBus.execute(
-  //     new GetCommentByIdOrInternalFailQuerySql(createdCommentId, undefined),
-  //   );
-  // }
-  //
+  @Get(':postId/comments')
+  @UseGuards(JwtAccessOptionalAuthGuardWrap)
+  async getPostComments(
+    @Param('postId', IntValidationPipe)
+    postId: string,
+    @Query() query: GetCommentsQueryParams,
+    @ExtractUserIfExistsFromRequest() user: UserContextDto | null,
+  ): Promise<PaginatedViewDto<CommentViewDto[]>> {
+    return this.queryBus.execute(
+      new GetPostCommentsQueryWrap(postId, query, user?.id),
+    );
+  }
+
+  @Post(':postId/comments')
+  @UseGuards(JwtAccessAuthGuardWrap)
+  async createPostComment(
+    @ExtractUserFromRequest() user: UserContextDto,
+    @Param('postId', IntValidationPipe)
+    postId: string,
+    @Body() body: CreatePostCommentInputDto,
+  ): Promise<CommentViewDto> {
+    const createdCommentId = await this.commandBus.execute<
+      CreateCommentCommandWrap,
+      string
+    >(
+      new CreateCommentCommandWrap({
+        content: body.content,
+        postId,
+        userId: user.id,
+      }),
+    );
+
+    return this.queryBus.execute(
+      new GetCommentByIdOrInternalFailQueryWrap(createdCommentId, undefined),
+    );
+  }
+
   // @Put(':postId/like-status')
   // @HttpCode(HttpStatus.NO_CONTENT)
   // @UseGuards(JwtAccessAuthGuardSql)
